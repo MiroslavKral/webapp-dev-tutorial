f09f2d27429b0d32837bcf689c46346216354aee
Miroslav Král <miroslav.kral.86@gmail.com>
2014-07-06 22:41:26 +0200

    Added styling for content and added functionality for first tab.

--- chapter4/example3/javascript/app.js
+++ chapter4/example3/javascript/app.js
@@ -6,7 +6,7 @@ var main = function () {
         "Finish reading this book",
         "Read more books about JavaScript",
         "Read more books about AngularJS",
-        "Write much more code a tests"
+        "Write much more code and tests"
     ];
 
 	$('.tabs a span').toArray().forEach(function (element) {
@@ -18,16 +18,9 @@ var main = function () {
 			$('main .content').empty();
 
             if($element.parent().is(':nth-child(1)')) {
-                console.log("First tab clicked!");
+                shotTodosFromNewest();
             } else if ($element.parent().is(":nth-child(2)")) {
-                console.log("Second tab clicked!");
-
-                var $content = $("<ul>");
-                toDos.forEach(function(todo){
-                    var $contentElement = $("<li>").text(todo);
-                    $content.append($contentElement);
-                });
-                $("main div.content").append($content);
+                showTodosFromOldest();
             } else if ($element.parent().is(":nth-child(3)")) {
                 console.log("Third tab clicked!");
             }
@@ -35,6 +28,26 @@ var main = function () {
 			return false;
 		});
 	});
+
+    var showTodosFromOldest = function() {
+        var $content = $("<ul>");
+        toDos.forEach(function(todo){
+            var $contentElement = $("<li>").text(todo);
+            $content.append($contentElement);
+        });
+        $("main div.content").append($content);
+    };
+
+    var shotTodosFromNewest = function() {
+        var $content = $("<ul>");
+
+        var index;
+        for (index = toDos.length - 1; index >= 0; index = index - 1) {
+            var $todoText = $("<li>").text(toDos[index]);
+            $content.append($todoText);
+        }
+        $("main div.content").append($content);
+    };
 
     $(".tabs a:first-child span").trigger("click");
 };
