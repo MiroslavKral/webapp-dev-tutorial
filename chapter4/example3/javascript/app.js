9f11472b31af01b42b654d508c3fcf8a62b1370c
Miroslav Král <miroslav.kral.86@gmail.com>
2014-07-06 11:26:58 +0200

    Adding chapter 3 example.

---
+++ chapter4/example3/javascript/app.js
@@ -0,0 +1,15 @@
+var main = function () {
+	"use strict";
+
+	$('.tabs a span').toArray().forEach(function (element) {
+		$(element).on('click', function() {
+			$('.tabs a span').removeClass('active');
+			$(element).addClass('active');
+			$('main .content').empty();
+			return false;
+		});
+	});
+
+};
+
+$(document).ready(main);
